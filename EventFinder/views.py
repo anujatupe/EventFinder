05406616c80e8e5af536263fc2925f199d859ca4
Anuja Tupe <anujatupe2012@gmail.com>
2015-03-21 22:25:24 -0700

    Changed the form method to get. Previous button on events page is disabled when you are on the first page.

--- EventFinder/views.py
+++ EventFinder/views.py
@@ -24,57 +24,57 @@ def home(request):
 
 @csrf_exempt
 def events(request):
-    if request.method == 'POST':
-        logger.debug("REQUEST OBJECT IS ***********")
-        logger.debug(request)
-        form = CategoriesForm(request.POST)
-        if form.is_valid():
-            g = GeoIP()
-            g.city(request.META['REMOTE_ADDR'])
-            geo_location = g.city('74.125.79.147')
-            request_params = urllib.urlencode({
-                                                "token" : "BJCBWSGK6STWD6FRC3UQ",
-                                                "categories" : ','.join(form.cleaned_data.get('category', [])),
-                                                "location.latitude" : geo_location['latitude'],
-                                                "location.longitude" : geo_location['longitude'],
-                                                "location.within" : "20mi",
-                                                "sort_by" : "date",
-                                                "page" : 1
-                                            })
-            events_url = "https://www.eventbriteapi.com/v3/events/search?"+request_params
-            request = urllib2.Request(events_url)
-            response = urllib2.urlopen(request)
-            resp_parsed = json.loads(response.read())
-            events_list = resp_parsed.get('events', None)
-            # logger.debug("FIRST EVENT:")
-            # logger.debug(events_list[0])
-            #   next_events_url = "?page=2&user_categories="+','.join(form.cleaned_data.get('category', []))
-            next_events_url = urllib.urlencode({
-                "page" : 2,
-                "user_categories" : ','.join(form.cleaned_data.get('category', []))
-            })
-            return render_to_response("EventFinderProject/events.html", {"events_list" : events_list, "next_events_url" : "?"+next_events_url })
-    elif request.method == 'GET':
+    logger.debug("REQUEST OBJECT IS ***********")
+    logger.debug(request)
+    form = CategoriesForm(request.GET)
+    if form.is_valid():
+        g = GeoIP()
+        g.city(request.META['REMOTE_ADDR'])
+        geo_location = g.city('74.125.79.147')
+        request_params = urllib.urlencode({
+                                            "token": "BJCBWSGK6STWD6FRC3UQ",
+                                            "categories": ','.join(form.cleaned_data.get('category', [])),
+                                            "location.latitude": geo_location['latitude'],
+                                            "location.longitude": geo_location['longitude'],
+                                            "location.within": "20mi",
+                                            "sort_by": "date",
+                                            "page": 1
+                                        })
+        events_url = "https://www.eventbriteapi.com/v3/events/search?"+request_params
+        request = urllib2.Request(events_url)
+        response = urllib2.urlopen(request)
+        resp_parsed = json.loads(response.read())
+        events_list = resp_parsed.get('events', None)
+        next_events_url = urllib.urlencode({
+            "page": 2,
+            "user_categories": ','.join(form.cleaned_data.get('category', []))
+        })
+        return render_to_response("EventFinderProject/events.html", {"events_list": events_list, "previous_events_url": "", "next_events_url": "?"+next_events_url, "previous": "disabled", "next": ""})
+    else:
         logger.debug("REQUEST OBJECT IS ***********")
         logger.debug(request)
         user_categories = request.GET.get('user_categories', '')
-        page = int(request.GET.get('page', '0')) + 1
-        #next_events_url = "?page="+str(page)+"&user_categories="+user_categories
+        page = int(request.GET.get('page', '0'))
+        previous_enabled = "disabled" if (page <= 1) else ""
+        previous_events_url = urllib.urlencode({
+                                "page": str(page - 1),
+                                "user_categories": user_categories
+        }) if (page > 0) else ""
         next_events_url = urllib.urlencode({
-            "page" : str(page),
-            "user_categories" : user_categories
+            "page": str(page + 1),
+            "user_categories": user_categories
         })
         g = GeoIP()
         g.city(request.META['REMOTE_ADDR'])
         geo_location = g.city('74.125.79.147')
         request_params = urllib.urlencode({
-                                            "token" : "BJCBWSGK6STWD6FRC3UQ",
-                                            "categories" : user_categories,
-                                            "location.latitude" : geo_location['latitude'],
-                                            "location.longitude" : geo_location['longitude'],
-                                            "location.within" : "20mi",
-                                            "sort_by" : "date",
-                                            "page" : page
+                                            "token": "BJCBWSGK6STWD6FRC3UQ",
+                                            "categories": user_categories,
+                                            "location.latitude": geo_location['latitude'],
+                                            "location.longitude": geo_location['longitude'],
+                                            "location.within": "20mi",
+                                            "sort_by": "date",
+                                            "page": page
                                         })
         events_url = "https://www.eventbriteapi.com/v3/events/search?"+request_params
         request = urllib2.Request(events_url)
@@ -83,7 +83,7 @@ def events(request):
         events_list = resp_parsed.get('events', None)
         # logger.debug("FIRST EVENT:")
         # logger.debug(events_list[0])
-        return render_to_response("EventFinderProject/events.html", {"events_list" : events_list, "next_events_url" : "?"+next_events_url })
+        return render_to_response("EventFinderProject/events.html", {"events_list" : events_list, "previous_events_url": "?"+previous_events_url, "next_events_url": "?"+next_events_url, "previous" : previous_enabled, "next" : "" })
     return render_to_response("EventFinderProject/events.html", "No name")
 
 
