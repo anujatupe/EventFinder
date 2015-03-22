cde1b86c4f15565a336ae0d45a66a593fbc5217b
Anuja Tupe <anujatupe2012@gmail.com>
2015-03-22 10:38:15 -0700

    Removed commented code from views.py. Pending - added templatestag folder and the files inside it

---
+++ EventFinder/templatetags/events_filter.py
@@ -0,0 +1,9 @@
+from django import template
+from datetime import datetime
+
+register = template.Library()
+
+@register.filter(name='events_date_filter')
+def events_date_filter(value):
+    time_obj = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S')
+    return time_obj
