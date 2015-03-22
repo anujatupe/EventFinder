0e51d2497b0cc64a7d50bd43d1b47c0947816ebd
Anuja Tupe <anujatupe2012@gmail.com>
2015-03-22 16:48:55 -0700

    Changed the link for name EventFinder in the navigation bar. Added basic test cases.

--- EventFinder/tests.py
+++ EventFinder/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
 
 # Create your tests here.
+class EventFinderViewsTestCase(TestCase):
+    def test_home(self):
+        resp = self.client.get('/')
+        self.assertTrue('form' in resp.context)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_events(self):
+        resp = self.client.get('/events')
+        self.assertTrue('next_events_url' in resp.context)
+        self.assertTrue('previous_events_url' in resp.context)
+        self.assertTrue('events_list' in resp.context)
+        self.assertTrue('previous' in resp.context)
+        self.assertTrue('next' in resp.context)
+        self.assertEqual(resp.status_code, 200)
