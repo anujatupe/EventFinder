5e78eeabc76cbcd7430f30821e974ae199d19ab8
Anuja Tupe <anujatupe2012@gmail.com>
2015-03-23 01:50:59 -0700

    Removed commented code from urls.py and added documentation for the function in events.js

--- EventFinderProject/urls.py
+++ EventFinderProject/urls.py
@@ -1,14 +1,6 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-# from django.conf import settings
-# from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'EventFinderProject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    #url(r'^admin/', include(admin.site.urls)
     url(r'^$', 'EventFinder.views.home', name='home'),
     url(r'events', 'EventFinder.views.events', name='events')
-) #+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
--- static/js/events.js
+++ static/js/events.js
@@ -1,4 +1,7 @@
 $(function(){
+  /*
+   * Function to disable a href if the attribute disabled is encountered in the a href element
+   */
   $('a').click(function()
     {
       return ($(this).attr('disabled')) ? false : true;
