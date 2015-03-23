49da6efe20a9819b5994479dee15ceef931232d9
Anuja Tupe <anujatupe2012@gmail.com>
2015-03-22 19:01:30 -0700

    Changed the indentation of the views.py . Added End date for event on the events page

--- EventFinder/views.py
+++ EventFinder/views.py
@@ -12,100 +12,100 @@ from django.template import *
 logger = logging.getLogger(__name__)
 
 def home(request):
-    """Route for the landing page - choose the categories for the events to be searched
+  """Route for the landing page - choose the categories for the events to be searched
 
-    Args:
-        request: request object
-    Returns:
-        Sends categories to the events page for getting the events related to the categories
-    """
+  Args:
+      request: request object
+  Returns:
+      Sends categories to the events page for getting the events related to the categories
+  """
 
-    form = CategoriesForm()
-    return render_to_response("EventFinderProject/home.html", {'form': form})
+  form = CategoriesForm()
+  return render_to_response("EventFinderProject/home.html", {'form': form})
 
 @csrf_exempt
 def events(request):
-    """Route for the events page - displays the events belonging to the category selected by the user
+  """Route for the events page - displays the events belonging to the category selected by the user
 
-    Args:
-        request: request object having get parameters. It has the category ids using which we search for events
-    Returns:
-        Renders a different page which has all the events related to the categories passed
-    """
+  Args:
+      request: request object having get parameters. It has the category ids using which we search for events
+  Returns:
+      Renders a different page which has all the events related to the categories passed
+  """
 
-    g = GeoIP()
-    ip_address = '74.125.79.147' if request.META['REMOTE_ADDR'] == '127.0.0.1' else request.META['REMOTE_ADDR']
-    geo_location = g.city(ip_address)
-    form = CategoriesForm(request.GET)
-    request_params = {
-                        "token": "BJCBWSGK6STWD6FRC3UQ",
-                        "location.latitude": geo_location['latitude'],
-                        "location.longitude": geo_location['longitude'],
-                        "location.within": "20mi",
-                        "sort_by": "date"
-                    }
-    if form.is_valid():
-        request_params.update({
-                                "categories": ','.join(form.cleaned_data.get('category', [])),
-                                "page": 1
-                            })
-        request_params = urllib.urlencode(request_params)
-        events_list, page_count = _get_events(request_params)
-        previous_events_url, next_events_url = _get_next_previous_event_urls(1, ','.join(form.cleaned_data.get('category', [])), 50)
-        next_enabled = "disabled" if page_count == 1 else ""
-        return render_to_response("EventFinderProject/events.html", {"events_list": events_list, "previous_events_url": previous_events_url, "next_events_url": "?"+next_events_url, "previous": "disabled", "next": next_enabled})
-    else:
-        user_categories = request.GET.get('user_categories', '')
-        page = int(request.GET.get('page', '0'))
-        previous_enabled = "disabled" if (page <= 1) else ""
-        previous_events_url, next_events_url = _get_next_previous_event_urls(page, user_categories, 50)
-        request_params.update({
-                                "categories": user_categories,
-                                "page": page
-                            })
-        request_params = urllib.urlencode(request_params)
-        events_list, page_count = _get_events(request_params)
-        next_enabled = "disabled" if page_count == page else ""
-        return render_to_response("EventFinderProject/events.html", {"events_list" : events_list, "previous_events_url": "?"+previous_events_url, "next_events_url": "?"+next_events_url, "previous" : previous_enabled, "next" : next_enabled })
+  g = GeoIP()
+  ip_address = '74.125.79.147' if request.META['REMOTE_ADDR'] == '127.0.0.1' else request.META['REMOTE_ADDR']
+  geo_location = g.city(ip_address)
+  form = CategoriesForm(request.GET)
+  request_params = {
+                      "token": "BJCBWSGK6STWD6FRC3UQ",
+                      "location.latitude": geo_location['latitude'],
+                      "location.longitude": geo_location['longitude'],
+                      "location.within": "20mi",
+                      "sort_by": "date"
+                  }
+  if form.is_valid():
+    request_params.update({
+                            "categories": ','.join(form.cleaned_data.get('category', [])),
+                            "page": 1
+                        })
+    request_params = urllib.urlencode(request_params)
+    events_list, page_count = _get_events(request_params)
+    previous_events_url, next_events_url = _get_next_previous_event_urls(1, ','.join(form.cleaned_data.get('category', [])), 50)
+    next_enabled = "disabled" if page_count == 1 else ""
+    return render_to_response("EventFinderProject/events.html", {"events_list": events_list, "previous_events_url": previous_events_url, "next_events_url": "?"+next_events_url, "previous": "disabled", "next": next_enabled})
+  else:
+    user_categories = request.GET.get('user_categories', '')
+    page = int(request.GET.get('page', '0'))
+    previous_enabled = "disabled" if (page <= 1) else ""
+    previous_events_url, next_events_url = _get_next_previous_event_urls(page, user_categories, 50)
+    request_params.update({
+                            "categories": user_categories,
+                            "page": page
+                        })
+    request_params = urllib.urlencode(request_params)
+    events_list, page_count = _get_events(request_params)
+    next_enabled = "disabled" if page_count == page else ""
+    return render_to_response("EventFinderProject/events.html", {"events_list" : events_list, "previous_events_url": "?"+previous_events_url, "next_events_url": "?"+next_events_url, "previous" : previous_enabled, "next" : next_enabled })
 
 
 def _get_events(request_params):
-    """Makes an API call to Eventbrite and gets all the events related to the categories that user wants
+  """Makes an API call to Eventbrite and gets all the events related to the categories that user wants
 
-    Args:
-        request_params: urlencoded dictionary of the get parameters to be sent to the API
-    Returns:
-        events_list: A list of dicts of the various events belonging to the category. Gets only the first 50 events.
-        Paginated response from Eventbrite API.
-        Also, returns the page count i.e. the total number of pages having the events.
-    """
+  Args:
+      request_params: urlencoded dictionary of the get parameters to be sent to the API
+  Returns:
+      events_list: A list of dicts of the various events belonging to the category. Gets only the first 50 events.
+      Paginated response from Eventbrite API.
+      Also, returns the page count i.e. the total number of pages having the events.
+  """
 
-    events_url = "https://www.eventbriteapi.com/v3/events/search?"+request_params
-    request = urllib2.Request(events_url)
-    response = urllib2.urlopen(request)
-    resp_parsed = json.loads(response.read())
-    events_list = resp_parsed.get('events', None)
-    return events_list, resp_parsed.get('pagination').get('page_count', 0)
+  events_url = "https://www.eventbriteapi.com/v3/events/search?"+request_params
+  request = urllib2.Request(events_url)
+  response = urllib2.urlopen(request)
+  resp_parsed = json.loads(response.read())
+  events_list = resp_parsed.get('events', None)
+  return events_list, resp_parsed.get('pagination').get('page_count', 0)
 
 def _get_next_previous_event_urls(page, user_categories, page_count):
-    """Creates the URL to get previous page's events and the url to get the next page's events.
+  """Creates the URL to get previous page's events and the url to get the next page's events.
 
-    Args:
-        page: current page number
-        user_categories: string of category ids selected by users. Category ids are separated by comma
-        page_count: Total number of pages having the events
-    Returns:
-        previous_events_url: URL to get the events of previous page
-        next_events_url: URL to get the events of next page
-    """
+  Args:
+      page: current page number
+      user_categories: string of category ids selected by users. Category ids are separated by comma
+      page_count: Total number of pages having the events
+  Returns:
+      previous_events_url: URL to get the events of previous page
+      next_events_url: URL to get the events of next page
+  """
 
-    previous_events_url = urllib.urlencode({
-                            "page": str(page - 1),
-                            "user_categories": user_categories
-    }) if (page > 1) else ""
-    next_events_url = urllib.urlencode({
-        "page": str(page + 1),
-        "user_categories": user_categories
-    }) if (page_count == 50) else ""
+  previous_events_url = urllib.urlencode({
+                          "page": str(page - 1),
+                          "user_categories": user_categories
+  }) if (page > 1) else ""
+  next_events_url = urllib.urlencode({
+      "page": str(page + 1),
+      "user_categories": user_categories
+  }) if (page_count == 50) else ""
 
-    return previous_events_url, next_events_url
+  return previous_events_url, next_events_url
