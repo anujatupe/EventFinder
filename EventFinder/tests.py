184f579537f471757fb40fdb41d19e449e87eb8c
Anuja Tupe <anujatupe2012@gmail.com>
2015-03-22 17:25:57 -0700

    Added test case for testing categories form

--- EventFinder/tests.py
+++ EventFinder/tests.py
@@ -1,17 +1,29 @@
 from django.test import TestCase
+from EventFinder.forms import CategoriesForm
 
 # Create your tests here.
-class EventFinderViewsTestCase(TestCase):
+class EventFinderTest(TestCase):
     def test_home(self):
-        resp = self.client.get('/')
-        self.assertTrue('form' in resp.context)
-        self.assertEqual(resp.status_code, 200)
+      resp = self.client.get('/')
+      self.assertTrue('form' in resp.context)
+      self.assertEqual(resp.status_code, 200)
 
     def test_events(self):
-        resp = self.client.get('/events')
-        self.assertTrue('next_events_url' in resp.context)
-        self.assertTrue('previous_events_url' in resp.context)
-        self.assertTrue('events_list' in resp.context)
-        self.assertTrue('previous' in resp.context)
-        self.assertTrue('next' in resp.context)
-        self.assertEqual(resp.status_code, 200)
+      resp = self.client.get('/events')
+      self.assertTrue('next_events_url' in resp.context)
+      self.assertTrue('previous_events_url' in resp.context)
+      self.assertTrue('events_list' in resp.context)
+      self.assertTrue('previous' in resp.context)
+      self.assertTrue('next' in resp.context)
+      self.assertEqual(resp.status_code, 200)
+
+    def test_valid_form(self):
+		  data = {'category': ['103','109'] }
+		  form = CategoriesForm(data=data)
+		  self.assertTrue(form.is_valid())
+
+
+
+
+
+
